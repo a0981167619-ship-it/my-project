e9ff123164021e0fc25a0aeb89ccc23b826b9e5e
K <a0981167619@gmail.com>
2026-07-02 13:12:55 +0800

    留言板-text

--- text-留言板.py
+++ text-留言板.py
@@ -1,9 +1,11 @@
 '''留言板'''
+import tkinter as tk
+victory=tk.Tk()
 def message():
     text4.insert(tk.END,entry.get()+'\n')
 
 word='留言板:'
-text4=tk.Text(victory,borderwidth=30,height=10)
+text4=tk.Text(victory,borderwidth=30,height=10,bg="#D8E4E6")
 text4.insert('1.0',word)
 text4.pack()
 
--- 文字區塊元件-text.py
+++ 文字區塊元件-text.py
@@ -14,18 +14,3 @@ text2.insert("1.1",aword)#在第一行的第一個位置插入字串
 text2.insert(tk.INSERT,'I think it is a perfect plan')
 text2.pack()
 
-
-'''每日代辦事項'''
-def added():
-    text3.insert(tk.END,entry.get()+'\n')
- 
-entry=tk.Entry(victory)
-entry.pack()
-
-text3=tk.Text(victory,borderwidth=30,height=30,wrap='char',bg='#FFF3BF',highlightbackground='#FFD6E0',highlightthickness=2)
-text3.pack()
-
-button=tk.Button(victory,text='增加',command=added)
-button.pack()
-
-victory.mainloop()
